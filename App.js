ba10d76a1ab7fe240cedaf8049881d011480a065
Samandar688 <isroilovs340@gmail.com>
2026-01-05 15:25:54 +0500

    feat: Integrate Mapbox GL JS, adding map display, required dependencies, and configuration, and updating Expo run scripts.

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Mapbox from '@rnmapbox/maps';
+
+// Public Access Tokenni shu yerga qo'ying
+Mapbox.setAccessToken('pk.eyJ1IjoiZG9taW5hbmQiLCJhIjoiY21qc2N1OGQ5MGhpOTNjcXdzaXJ6cjdxbyJ9.MzO3-7XIy1HPd7jC0ka07g');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Mapbox.MapView style={styles.map}>
+        <Mapbox.Camera
+          zoomLevel={12}
+          centerCoordinate={[69.2401, 41.2995]} // Toshkent
+        />
+      </Mapbox.MapView>
     </View>
   );
 }
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
   },
 });
